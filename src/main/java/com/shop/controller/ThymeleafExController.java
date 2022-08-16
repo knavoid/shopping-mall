51044bf8181f95dc1d266ed84a2b3864e0dafb27
knavoid <k_shun@naver.com>
2022-08-16 18:33:33 +0900

    thymeleaf 환경 설정 및 예시 controller 생성

---
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,16 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping(value = "/example")
+    public String thymeleafExample07(){
+        return "example";
+    }
+
+}
