c95ce4d5341b5017913b64ad631718efab2de078
knavoid <k_shun@naver.com>
2022-09-07 18:03:45 +0900

    상품 및 이미지 정보 수정 기능 구현

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -9,7 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -33,6 +35,24 @@ public class ItemImgService {
         // 상품 이미지 정보 저장
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws IOException {
+
+        ItemImg itemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+        // 기존 이미지 파일 삭제
+        if (!StringUtils.isEmpty(itemImg.getImgName())) {
+            fileService.deleteFile(itemImgLocation + "/" + itemImg.getImgName());
+        }
+
+        // 이미지 파일 업로드
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = fileService.uploadFile(itemImgLocation, Objects.requireNonNull(oriImgName), itemImgFile.getBytes());
+        String imgUrl = "/images/item/" + imgName;
+
+        // 상품 이미지 정보 저장
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
     }
 
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -40,6 +43,38 @@ public class ItemService {
                 itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
             }
         }
+        return item.getId();
+    }
+
+    public ItemFormDto getItemDetail(Long itemId) {
+        // 이미지 찾기
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            itemImgDtoList.add(ItemImgDto.of(itemImg));
+        }
+
+        // 상품 찾기
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+
+        return itemFormDto;
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws IOException {
+        // 상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId()).orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto);
+
+        // 상품 이미지 수정
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            if (!itemImgFileList.get(i).isEmpty()) {
+                itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
+            }
+        }
+
         return item.getId();
     }
 
